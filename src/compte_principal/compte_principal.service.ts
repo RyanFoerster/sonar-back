8fb596d988a16ea4248fcc705bc2bb1c4d7f34e4
RyanFoerster <ryanfoerster@outlook.be>
2024-07-21 12:52:38 +0200

    modification pour module facture

--- src/compte_principal/compte_principal.service.ts
+++ src/compte_principal/compte_principal.service.ts
@@ -1,55 +1,63 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { CreateComptePrincipalDto } from "./dto/create-compte_principal.dto";
-import { UpdateComptePrincipalDto } from "./dto/update-compte_principal.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { ComptePrincipal } from "./entities/compte_principal.entity";
-import { Repository } from "typeorm";
-import { CompteGroupeService } from "src/compte_groupe/compte_groupe.service";
-import { CompteGroupe } from "src/compte_groupe/entities/compte_groupe.entity";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { CreateComptePrincipalDto } from './dto/create-compte_principal.dto';
+import { UpdateComptePrincipalDto } from './dto/update-compte_principal.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ComptePrincipal } from './entities/compte_principal.entity';
+import { Repository } from 'typeorm';
+import { CompteGroupeService } from 'src/compte_groupe/compte_groupe.service';
+import { CompteGroupe } from 'src/compte_groupe/entities/compte_groupe.entity';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class ComptePrincipalService {
   constructor(
-    @InjectRepository(ComptePrincipal) private readonly comptePrincipalRepository: Repository<ComptePrincipal>,
-    private readonly compteGroupeService: CompteGroupeService
-  ){}
+    @InjectRepository(ComptePrincipal)
+    private readonly comptePrincipalRepository: Repository<ComptePrincipal>,
+    private readonly compteGroupeService: CompteGroupeService,
+    //private readonly usersService: UsersService,
+  ) {}
 
   async create(createComptePrincipalDto: CreateComptePrincipalDto) {
-    const compteGroupe: CompteGroupe | null= await this.compteGroupeService.findOneByUsername(createComptePrincipalDto.username)
-    if(compteGroupe!==null){
-      throw new UnauthorizedException("Ce nom de compte est déjà utilisé")
+    const compteGroupe: CompteGroupe | null =
+      await this.compteGroupeService.findOneByUsername(
+        createComptePrincipalDto.username,
+      );
+    if (compteGroupe !== null) {
+      throw new UnauthorizedException('Ce nom de compte est déjà utilisé');
     } else {
-      return this.comptePrincipalRepository.save(createComptePrincipalDto)
-    }}
-
-  findAll() {
-    return this.comptePrincipalRepository.find(); }
-
-  findOne(id: number) {
-    return this.comptePrincipalRepository.findOne({
-      where: {
-        id
-      }
-  });
+      return this.comptePrincipalRepository.save(createComptePrincipalDto);
+    }
   }
 
-  findOneWithoutRelation(id: number){
+  findAll() {
+    return this.comptePrincipalRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this.comptePrincipalRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  findOneWithoutRelation(id: number) {
     return this.comptePrincipalRepository.findOne({
       where: {
-        id
+        id,
       },
       relations: {
         invoice: false,
         quote: false,
-      }
-    })
+      },
+    });
   }
 
-  findOneByUsername(username: string){
-    return this.comptePrincipalRepository.findOneBy({username});
+  findOneByUsername(username: string) {
+    return this.comptePrincipalRepository.findOneBy({ username });
   }
 
   update(id: number, updateComptePrincipalDto: UpdateComptePrincipalDto) {
-    return this.comptePrincipalRepository.update(id, updateComptePrincipalDto)
+    return this.comptePrincipalRepository.update(id, updateComptePrincipalDto);
   }
 }
