bc3fbc6434326565ef160106befc9f4c1ca6e894
RyanFoerster <ryanfoerster@outlook.be>
2024-08-07 19:29:18 +0200

    Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,61 +1,61 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AuthModule } from "./auth/auth.module";
-import { UsersModule } from "./users/users.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { ComptePrincipalModule } from "./compte_principal/compte_principal.module";
-import { CompteGroupeModule } from "./compte_groupe/compte_groupe.module";
-import { UserSecondaryAccountModule } from "./user-secondary-account/user-secondary-account.module";
-import { ProductModule } from "./product/product.module";
-import { InvoiceModule } from "./invoice/invoice.module";
-import { ClientsModule } from "./clients/clients.module";
-import { QuoteModule } from "./quote/quote.module";
-import { ScheduleModule } from "@nestjs/schedule";
-import { TransactionModule } from "./transaction/transaction.module";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
-import { EventsModule } from "./event/event.module"; 
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ComptePrincipalModule } from './compte_principal/compte_principal.module';
+import { CompteGroupeModule } from './compte_groupe/compte_groupe.module';
+import { UserSecondaryAccountModule } from './user-secondary-account/user-secondary-account.module';
+import { ProductModule } from './product/product.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { ClientsModule } from './clients/clients.module';
+import { QuoteModule } from './quote/quote.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TransactionModule } from './transaction/transaction.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { EventsModule } from './event/event.module';
 import { InvitationsModule } from './invitation/invitation.module';
 import { CommentsModule } from './comment/comment.module';
-import { JwtModule } from "@nestjs/jwt";
+import { JwtModule } from '@nestjs/jwt';
 import { VirementSepaModule } from './virement-sepa/virement-sepa.module';
-import config from "./config/config";
+import config from './config/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [config]
+      load: [config],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: "mysql",
-        database: configService.get("database.database"),
-        host: configService.get("database.host"),
-        port: +configService.get("database.port"),
-        username: configService.get("database.username"),
-        password: configService.get("database.password"),
-        entities: [__dirname + "/**/*.entity{.ts,.js}"],
-        synchronize: true
+        type: 'postgres',
+        database: configService.get('database.database'),
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
       }),
-      inject: [ConfigService]
+      inject: [ConfigService],
     }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get("jwt.secret")
+        secret: configService.get('jwt.secret'),
       }),
       inject: [ConfigService],
-      global: true
+      global: true,
     }),
     ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "uploads"), // Indiquez le chemin vers le dossier des fichiers uploadés
-      serveRoot: "/uploads" // Le préfixe de l'URL pour accéder aux fichiers
+      rootPath: join(__dirname, '..', 'uploads'), // Indiquez le chemin vers le dossier des fichiers uploadés
+      serveRoot: '/uploads', // Le préfixe de l'URL pour accéder aux fichiers
     }),
     AuthModule,
     UsersModule,
@@ -70,11 +70,9 @@ import config from "./config/config";
     EventsModule,
     InvitationsModule,
     CommentsModule,
-    VirementSepaModule
-
+    VirementSepaModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
