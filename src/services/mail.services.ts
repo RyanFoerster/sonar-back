0f2c1ed5fa304024e87f7a165e4b6c103a970175
RyanFoerster <ryanfoerster@outlook.be>
2024-09-02 09:25:09 +0200

    change password reset

--- src/services/mail.services.ts
+++ src/services/mail.services.ts
@@ -1,20 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import * as nodemailer from "nodemailer";
+import { Injectable, Logger } from "@nestjs/common";
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from "@nestjs/config";
+import { HttpService } from "@nestjs/axios";
 
 @Injectable()
 export class MailService {
-
   private transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
       user: 'vernice23@ethereal.email',
-      pass: '6yW3nPBuv5jvYPrGwt'
-    }
+      pass: '6yW3nPBuv5jvYPrGwt',
+    },
   });
 
-  async sendPasswordResetEmail(to: string, token: string) {
-    const resetLink = `http://yourapp.com/reset-password?token=${token}`;
+  constructor(private readonly configService: ConfigService,
+              private readonly httpService: HttpService) {
+  }
+
+  async sendPasswordResetEmail(to: string, token: string, firstName: string, name: string) {
+    /*const resetLink = `http://yourapp.com/reset-password?token=${token}`;
     const mailOptions = {
       from: 'Auth-backend service',
       to,
@@ -22,6 +27,26 @@ export class MailService {
       html: `<p>You requested a password reset. Click the link below to reset your password:</p><p><a href="${resetLink}">Reset Password</a></p>`
     }
 
-    await this.transporter.sendMail(mailOptions);
+    await this.transporter.sendMail(mailOptions);*/
+    Logger.debug("Je passe ici")
+    Logger.debug(this.configService.get('mailhub.api_key'))
+    try {
+      this.httpService.post(`https://api.mailhub.sh/v1/send`, {
+        layout_identifier: 'tp-dc76ec2fba7f4b04',
+        variables: {
+          firstName,
+          name,
+          resetToken: token,
+        },
+        from: '@sonarartistsapp@gmail.com',
+        to,
+        subject: 'Réinitialisation du mot de passe',
+        language: null,
+      }).subscribe(data => console.log(data))
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    
   }
 }
