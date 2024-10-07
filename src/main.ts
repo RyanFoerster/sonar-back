c29ccdf62e8c8c33010ed98cb39cfaec585cf236
RyanFoerster <ryanfoerster@outlook.be>
2024-10-07 10:57:55 +0200

    FIX vercel issue

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getConnection } from 'typeorm';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    snapshot: true,
-    abortOnError: false,
-  });
+const server = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+    {
+      snapshot: true,
+      abortOnError: false,
+    },
+  );
 
   app.enableCors({
     origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.init();
+  return app;
+};
+
+let app: any;
+
+export default async (req: express.Request, res: express.Response) => {
+  if (!app) {
+    app = await createNestServer(server);
+    await app.init();
+  }
+  server(req, res);
+};
+
+// Pour le développement local
+if (process.env.NODE_ENV !== 'production') {
+  createNestServer(server)
+    .then((app) => app.listen(process.env.PORT || 3000))
+    .then(() => console.log('Server is running on http://localhost:3000'))
+    .catch((err) => console.error('Error starting server', err));
 }
-bootstrap();
