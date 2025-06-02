1421dfce362d49ae5c6ae9e933ce734fb2debf81
ryanfoerster <ryanfoerster@outlook.be>
2025-06-02 14:40:11 +0200

    🔧 refactor(migrations): correction des noms de colonnes pour les dates de meetup
    - Mise à jour des colonnes "meetup_date_time" et "end_date_time" en "meetupDateTime" et "endDateTime" respectivement pour assurer la cohérence avec la convention de nommage.
    - Les colonnes sont désormais définies comme nullable.

--- src/migrations/1748862860164-MeetupDateTimeOptional.ts
+++ src/migrations/1748862860164-MeetupDateTimeOptional.ts
@@ -4,10 +4,10 @@ export class MeetupDateTimeOptional1748862860164 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // S'assurer que les colonnes meetupDateTime et endDateTime sont définies comme nullable
     await queryRunner.query(
-      `ALTER TABLE "event" ALTER COLUMN "meetup_date_time" DROP NOT NULL`,
+      `ALTER TABLE "event" ALTER COLUMN "meetupDateTime" DROP NOT NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE "event" ALTER COLUMN "end_date_time" DROP NOT NULL`,
+      `ALTER TABLE "event" ALTER COLUMN "endDateTime" DROP NOT NULL`,
     );
   }
 
